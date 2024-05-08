97c81640e6cda9bd96be168450e0d2c6e81948cc
OURO-BANG'NA <ourobadiou@gmail.com>
2024-05-08 23:24:14 +0200

    Update make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://flask-ml-azure.azurewebsites.net:$PORT/predict 
+     -X POST https://lask-ml-service-badiou.azurewebsites.net:$PORT/predict 
      # TODO: Replace the <yourappname> with your application name 
